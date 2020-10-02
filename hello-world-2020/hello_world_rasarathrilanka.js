f0dc1354cda70c09a3d70e1a447782a7bc9c14f0
Rasara Thrilanka <36699207+rasarathrilanka@users.noreply.github.com>
2020-10-02 20:08:39 +0530

    Create hello_world_rasarathrilanka.js

---
+++ hello-world-2020/hello_world_rasarathrilanka.js
@@ -0,0 +1,14 @@
+// AUTHOR: Rasara Thrilanka
+// LANGUAGE: JavaScript
+// GITHUB: https://github.com/rasarathrilanka
+
+<!DOCTYPE HTML>
+<html>
+<body>
+  <script>
+    alert( 'Hello, world!' );
+    <!-- console.log("Hello, World!"); -->
+    <!-- document.write('Hello, world!'); -->
+  </script>
+</body>
+</html>
