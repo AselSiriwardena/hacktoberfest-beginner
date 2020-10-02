61772a6f363b299a9d652ee3e65f96015352efc4
Haider <haider.haider168@gmail.com>
2020-10-02 19:18:43 +0530

    rename file

---
+++ hello-world-2020/hello_world_mohdhaider.java
@@ -0,0 +1,9 @@
+// AUTHOR: Mohd Haider
+// LANGUAGE: Java
+// GITHUB: https://github.com/mhaider110
+
+class HelloWorldMohdHaider {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
