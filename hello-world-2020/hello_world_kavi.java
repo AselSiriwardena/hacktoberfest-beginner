ea78cb83de784d05d8a07160e71eb6796b4e700a
kavithriwijenayake <72968691+kavithriwijenayake@users.noreply.github.com>
2020-10-16 15:04:13 +0530

    Create hello_world_kavi.java

---
+++ hello-world-2020/hello_world_kavi.java
@@ -0,0 +1,9 @@
+// AUTHOR: Kavithri Wijenayake
+// LANGUAGE: Java
+// GITHUB: https://github.com/kavithriwijenayake
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
