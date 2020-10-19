902051727c7a35d7bd8a017fe590b8fbf347df50
Jean de Lima Lopes <jean.llopes@outlook.com>
2020-10-19 02:58:02 +0000

    Add file hello_world_jeanllopes.js

---
+++ hello-world-2020/hello_world_jeanllopes.js
@@ -0,0 +1,12 @@
+// AUTHOR: Jean Lopes
+// LANGUAGE: JavaScript
+// GITHUB: https://github.com/JeanLLopes
+
+<!DOCTYPE HTML>
+<html>
+<body>
+  <script>
+    alert( 'Hello, world!');
+  </script>
+</body>
+</html>
