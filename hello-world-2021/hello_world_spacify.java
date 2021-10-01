65f75f82b6bc788be29d4d0ab88080405b0cb53b
SPACIFY <73402363+FTS18@users.noreply.github.com>
2021-10-01 23:12:05 +0530

    Create hello_world_spacify.java

---
+++ hello-world-2021/hello_world_spacify.java
@@ -0,0 +1,9 @@
+//AUTHOR: SPACIFY
+//LANGUAGE: JAVA
+//GITHUB: https://github.com/fts18
+
+public class hello_world_spacify {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
