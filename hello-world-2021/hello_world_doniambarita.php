8f4c9a1e3cae0a6145b47b7e48925596531c4364
DoniGanteng <69050120+doniambarita@users.noreply.github.com>
2021-10-01 21:24:20 +0700

    Create hello_world_doniambarita.php

---
+++ hello-world-2021/hello_world_doniambarita.php
@@ -0,0 +1,16 @@
+<?php
+
+// AUTHOR: Doni Ambarita
+// LANGUAGE: PHP
+// GITHUB: https://github.com/doniambarita
+
+
+// Using echo
+echo "hello world"
+// No newline or space is added; the below outputs "helloworld" all on one line
+echo "hello";
+echo "world";
+
+// for more : https://www.php.net/manual/en/function.echo
+
+?>
