0603787583a9258c93fb7d0d45fcecad00bb5c6b
Dias Utsman <67829721+diasdev15@users.noreply.github.com>
2021-10-01 14:11:23 +0700

    Commit message

--- hello-world-2020/hello_world_andreasdaskalopoulos.js
+++ hello-world-2020/hello_world_andreasdaskalopoulos.js
@@ -1,3 +1,3 @@
 const hello = () => "Hello World!";
 
-hello();
+alert(hello())
--- hello-world-2020/hello_world_camezza.js
+++ hello-world-2020/hello_world_camezza.js
@@ -1,2 +1,3 @@
 // Node.js hello world
 console.log("Hello, world!"); // That's it
+for(let i = 0;i < 10;i++) console.log("Hello, world!"); // That's it
