3db8a1a25044385d9fbc29b2ad3e839e23a0445a
husinassegaff <husinassegaff15@gmail.com>
2021-10-02 05:43:50 +0700

    create Hello World in Java language

---
+++ hello-world-2021/hello_world_husinassegaff.java
@@ -0,0 +1,9 @@
+//AUTHOR:   husinassegaff
+//LANGUAGE: Java
+//GITHUB: https://github.com/husinassegaff
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
