898a23c3edc371fb1c5e8c3a70fc96d7380e05cb
Hrishabh Jain <hrishabh507@gmail.com>
2021-10-03 12:39:45 +0530

    Add hello world program in cpp

---
+++ hello-world-2021/hello_world_hrishabhjain.cpp
@@ -0,0 +1,14 @@
+/*
+AUTHOR: Hrishabh Jain
+LANGUAGE: C++
+GITHUB: https://github.com/Hrishabh5
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
