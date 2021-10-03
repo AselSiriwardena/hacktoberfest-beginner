f677e3dc229bb45177960868f7d0e3dc3bd7a8c3
Haikel <77146709+haikelz@users.noreply.github.com>
2021-10-03 14:28:19 +0700

    Create hello_world_haikelz.go

---
+++ hello-world-2021/hello_world_haikelz.go
@@ -0,0 +1,15 @@
+// AUTHOR: Haikel
+// LANGUAGE: Go
+// GITHUB: https://github.com/haikelz
+
+package main
+
+import "fmt"
+
+func main() {
+	var sisi1 = 100
+	var sisi2 = 2
+	var hasil = sisi1 / sisi2
+
+	fmt.Println("Hello world! 100 / 2 =", hasil)
+}
