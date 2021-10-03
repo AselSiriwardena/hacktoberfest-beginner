f65ca6a8be3a96842539d63e623051c7efad28bf
Rohat-Iftikhar <rohatiftikhar2000@gmail.com>
2021-10-03 21:09:49 +0500

    Created hello_world_rohat-iftikhar.java

---
+++ hello-world-2021/hello_world_rohat-iftikhar.java
@@ -0,0 +1,9 @@
+// AUTHOR: Rohat Iftikhar
+// LANGUAGE: Java
+// GITHUB: https://github.com/Rohat-Iftikhar
+
+class Hello{
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+	}
+}
