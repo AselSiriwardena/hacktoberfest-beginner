72352ca450c2a20fe78c4b21f8f69878b0867f78
anukindipa <61876990+anukindipa@users.noreply.github.com>
2021-10-05 09:19:12 +0530

    created a hello world
    added hello world

---
+++ hello-world-2021/hello_world_anuk.py
@@ -0,0 +1,6 @@
+# AUTHOR: Anuk Indipa Gamage
+# LANGUAGE: Python
+# GITHUB: https://github.com/anukindipa
+# Thank You for having me. This is the first pull request i made toa repo which isn't mine
+
+print("Hello World!")
