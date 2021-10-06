b32e800d4aae060552950cfadd9046fede723474
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2021-10-06 21:25:34 +0200

    Create hello_world_eirik.cs

---
+++ hello-world-2021/hello_world_eirik.cs
@@ -0,0 +1,16 @@
+// AUTHOR: Eirik Sjøløkken
+// LANGUAGE: C#  
+// GITHUB: https://github.com/Eiromplays
+
+using System;
+
+namespace Hello.World
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
