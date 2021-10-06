7aa54f9fe4541fb9bb248e1a3f20c547aac7fc88
abhay-420 <abhay2001kapatia@gmail.com>
2021-10-07 01:59:36 +0530

    created Hello world

---
+++ hello-world-2021/hello_world_abhaykapatia.java
@@ -0,0 +1,8 @@
+// AUTHOR: Abhay Kapatia
+// LANGUAGE: JAVA
+// GITHUB: https://github.com/abhay-420
+public class hello_world_abhaykapatia{
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+    }
+}
