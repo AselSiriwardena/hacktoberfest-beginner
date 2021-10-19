a85536b3e42cf4ba5409f6346a87dfbc9eece117
TanCL88 <65444711+TanCL88@users.noreply.github.com>
2021-10-19 20:00:02 +0800

    Create hello_world_TanCL88.cpp

---
+++ hello-world-2021/hello_world_TanCL88.cpp
@@ -0,0 +1,15 @@
+//Author: TanCL88
+//Language : C++
+//Github: https://github.com/TanCL88
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"----------------/n";
+    cout<<"2021 Hello World/n";
+    cout<<"----------------/n";
+    return 0;
+}
