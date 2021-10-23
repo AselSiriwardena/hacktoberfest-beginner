ec49e351e3819a0a027bb515f9a972c997a5df9b
Vicky398 <89054315+Vicky398@users.noreply.github.com>
2021-10-23 08:46:05 +0530

    Create Hello_world_Vicky398.js

---
+++ hello-world-2021/Hello_world_Vicky398.js
@@ -0,0 +1,7 @@
+//Made By: Vicky398
+//Language: JavaScript
+//Github: https://github.com/Vicky398
+
+let str = ['H','e','l','l','o','W','o','r','l','d'];
+console.log(str.join(''));
+ //Output: HelloWorld
