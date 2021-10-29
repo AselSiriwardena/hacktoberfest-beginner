84b42746a8c646c63f904c3e51e3ea53a072d05b
Asel Siriwardena <35985674+AselSiriwardena@users.noreply.github.com>
2021-10-29 07:23:09 -0700

    Revert "Added HelloWorld In javascript"

--- hello-world-2021/Hello_world_Vicky398.js
+++
@@ -1,7 +0,0 @@
-//Made By: Vicky398
-//Language: JavaScript
-//Github: https://github.com/Vicky398
-
-let str = ['H','e','l','l','o','W','o','r','l','d'];
-console.log(str.join(''));
- //Output: HelloWorld
