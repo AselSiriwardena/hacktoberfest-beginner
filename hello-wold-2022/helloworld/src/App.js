1b03c13496637cb49aea120010bbe5e21fc1ca0d
Manaswini123456 <janu2001dec@gmail.com>
2022-10-12 00:58:02 +0530

    Hello World

---
+++ hello-wold-2022/helloworld/src/App.js
@@ -0,0 +1,20 @@
+//AUTHOR : Manaswini123456
+//LANGUAGE : JAVASCRIPT , LIBRARY:REACTJS
+//GITHUB : https://github.com/Manaswini123456
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>HELLO WORLD!</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
